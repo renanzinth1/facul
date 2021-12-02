9a33a0abcc265d7b20529961d8fb8a4cae712140
Renan Narciso <renan.narciso@unigranrio.br>
2021-12-01 21:27:44 -0300

    Update passivo.py

--- passivo.py
+++ passivo.py
@@ -1,28 +1,31 @@
-#lado passivo
+# lado passivo
 import socket
 
-HOST = '' #interface padrão de comunicação da máquina
-PORTA = 5000 #identifica o processo na máquina
+HOST = ''  # interface padrão de comunicação da máquina
+PORTA = 5000  # identifica o processo na máquina
 
-#cria o descritor socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #internet e TCP
+# cria o descritor socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # internet e TCP
 
-#vincular o endereço e porta
+# vincular o endereço e porta
 sock.bind((HOST, PORTA))
 
-#colocar em modo de despera
-sock.listen(1) #argumento indica a qtde de conexões pendentes
+# colocar em modo de despera
+sock.listen(1)  # argumento indica a qtde de conexões pendentes
 
-#aceitar conexão
+# aceitar conexão
 novoSock, endereco = sock.accept()
 print('Conectado com:' + str(endereco))
 
 while True:
-    #esperar por mensagem do lado ativo
-    msg = novoSock.recv(1024) #argumento indica qtde máxima de bytes
+    # esperar por mensagem do lado ativo
+    msg = novoSock.recv(1024)  # argumento indica qtde máxima de bytes
     if not msg: break
     print(str(msg, encoding='utf-8'))
-    novoSock.send(b'Ola, sou o lado passivo!')
-    
-#fechar o descritor de socket da conexão
+
+    # retorna mensagem recebida para o lado ativo
+    novoSock.send(msg)
+
+# fechar o descritor de socket da conexão
 novoSock.close()
+print("\n\n\nConexão interrompida...")
