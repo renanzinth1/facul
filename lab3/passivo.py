e6ffa6b55c2f9ba6ac168f5882a997551d2e5539
Renan Narciso <renan.narciso@unigranrio.br>
2022-01-10 16:10:42 -0300

    Update passivo.py

--- lab3/passivo.py
+++ lab3/passivo.py
@@ -1,14 +1,76 @@
 # lado passivo
 
 import os.path
+import socket
 import select
 import sys
 import multiprocessing
 
-from lab3.RequestService import iniciaServidor, aceitaConexao, atendeRequisicoes
-
 entradas = [sys.stdin]
 conexoes = {}
+
+HOST = ''  # interface padrão de comunicação da máquina
+PORTA = 10000  # identifica o processo na máquina
+
+def iniciaServidor():
+    # cria o descritor socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # internet e TCP
+
+    # vincular o endereço e porta
+    sock.bind((HOST, PORTA))
+
+    # colocar em modo de despera
+    sock.listen(7)  # argumento indica a qtde de conexões pendentes
+
+    sock.setblocking(False)
+
+    entradas.append(sock)
+    return sock
+
+def aceitaConexao(sock):
+    # aceitar conexão
+    novoSock, addr = sock.accept()
+
+    # registra a nova conexao
+    conexoes[novoSock] = addr
+
+    return novoSock, addr
+
+def atendeRequisicoes(novoSock, addr):
+    while True:
+        # recebe o nome do documento vindo do lado do cliente
+        nomeArquivoRecv = novoSock.recv(1024)
+
+        # recebe a palavra a ser buscada vindo do lado cliente
+        palavraBuscadaRecv = novoSock.recv(1024)
+
+        # pega o diretório atual
+        # diretorioAtual = os.getcwd()
+
+        # uni o diretório atual com o nome do arquivo, formando um caminho absoluto
+        # filePath = os.path.join(diretorioAtual, nomeArquivoRecv)
+
+        if not nomeArquivoRecv:
+            print(str(addr) + '-> encerrou')
+            novoSock.close()  # encerra a conexao com o cliente
+            return
+        else:
+            if os.path.isfile(nomeArquivoRecv):
+                arquivo = open(nomeArquivoRecv, mode='r')
+                conteudo = arquivo.read()
+                qntdBusca = conteudo.count(str(palavraBuscadaRecv, encoding='utf-8'))
+
+                if qntdBusca > 0:
+                    print(qntdBusca)
+                    result = "Quantidade da palavra buscada encontrada no arquivo: " + str(qntdBusca) + " vez(es)."
+                else:
+                    result = "Palavra não encontrada no arquivo"
+                arquivo.close()
+            else:
+                result = "Arquivo não encontrado!"
+
+        # envia mensagem de resposta para o lado do cliente
+        novoSock.send(bytes(result, 'utf-8'))
 
 def main():
 
